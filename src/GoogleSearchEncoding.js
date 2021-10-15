00297bdf9b8942f7a13942c5b98c430feed5367d
David Xie <xdkdavid@gmail.com>
2021-10-14 21:29:18 -0700

    implement YouTube search query extraction" ;

---
+++ src/GoogleSearchEncoding.js
@@ -0,0 +1,85 @@
+// THIS FILE IS NOT USED
+// This arrays of object stores relationship between original character and decode Google's search URL query (https://developers.google.com/custom-search/docs/xml_results_appendices?hl=en#url-escaping)
+
+export default GoogleSearchEncoding = [
+    {
+        "Character": " ",
+        "GoogleEncoding": "+"
+    },
+    {
+        "Character": "$",
+        "GoogleEncoding": "24%"
+    },
+    {
+        "Character": "-",
+        "GoogleEncoding": "%2D"
+    },
+    {
+        "Character": "_",
+        "GoogleEncoding": "%5F"
+    },
+    {
+        "Character": ".",
+        "GoogleEncoding": "%2E"
+    },
+    {
+        "Character": "+",
+        "GoogleEncoding": "%2B"
+    },
+    {
+        "Character": "!",
+        "GoogleEncoding": "21%"
+    },
+    {
+        "Character": "*",
+        "GoogleEncoding": "%2A"
+    },
+    {
+        "Character": "\"",
+        "GoogleEncoding": "22%"
+    },
+    {
+        "Character": "'",
+        "GoogleEncoding": "27%"
+    },
+    {
+        "Character": "(",
+        "GoogleEncoding": "28%"
+    },
+    {
+        "Character": ")",
+        "GoogleEncoding": "29%"
+    },
+    {
+        "Character": ";",
+        "GoogleEncoding": "%3B"
+    },
+    {
+        "Character": "/",
+        "GoogleEncoding": "%2F"
+    },
+    {
+        "Character": "?",
+        "GoogleEncoding": "%3F"
+    },
+    {
+        "Character": ":",
+        "GoogleEncoding": "%3A"
+    },
+    {
+        "Character": "@",
+        "GoogleEncoding": "40%"
+    },
+    {
+        "Character": "=",
+        "GoogleEncoding": "%3D"
+    },
+    {
+        "Character": "&",
+        "GoogleEncoding": "26%"
+    },
+    {
+        "Character": "|",
+        "GoogleEncoding": "%7C"
+    }
+]
--- src/PageVisitingTrackerModule.js
+++ src/PageVisitingTrackerModule.js
@@ -4,29 +4,50 @@
 
 import * as webScience from "@mozilla/web-science";
 
-// For analytical purpose, YouTube will embed extra information in its url link, which will make us challenging to find out whether a user opens a same video.
 // This helper function will standardize YouTube URLs
-// For Videos, we can use
-// https://www.youtube.com/watch?t=230&v=MuOe3NM_2Ig&feature=youtu.be -> MuOe3NM_2Ig
+// It will categorize YouTube page URL, such as home, search, video, and other
 const standardizeYouTubeURL = (originalYouTubeURL) => {
-    let returnValue = {
+    const returnValue = {
         content: "",
         type: ""
     }
     // We encountered a YouTube Video URL
     if(originalYouTubeURL.includes('/watch?')) {
-      const videoIdentifierStartIndex = originalYouTubeURL.indexOf("v=");
-      let videoIdentifier = "";
-      if (originalYouTubeURL.includes("&", videoIdentifierStartIndex)) {
-          const videoIdentifierEndIndex = originalYouTubeURL.indexOf("&", videoIdentifierStartIndex);
-          videoIdentifier = originalYouTubeURL.slice(videoIdentifierStartIndex, videoIdentifierEndIndex);
-      } else {
-          videoIdentifier = originalYouTubeURL.slice(videoIdentifierStartIndex);
-      }
-      returnValue.content = videoIdentifier;
+      returnValue.content = extractYouTubeVideoID(originalYouTubeURL);
       returnValue.type = "Video";
+    } 
+    
+    // We encountered a YouTube Search URL
+    else if(originalYouTubeURL.includes('search_query=')) {
+        returnValue.content = extractYouTubeVideoID(originalYouTubeURL);
+        returnValue.type = "Search";
     }
+    
     return returnValue;
+}
+
+// Extract YouTube's search query from YouTube's search query URL, taking account to URL encoding characters
+// https://www.youtube.com/watch?t=230&v=MuOe3NM_2Ig&feature=youtu.be -> MuOe3NM_2Ig
+// https://www.youtube.com/results?search_query=MC-21+300 -> MC-21 300
+const extractYouTubeVideoID = (YouTubeURL) => {
+    const searchIdentifierStartIndex = YouTubeURL.indexOf("search_query=") + 13;
+    let searchQuery = "";
+    if (YouTubeURL.includes("&", searchIdentifierStartIndex)) {
+        const videoIdentifierEndIndex = YouTubeURL.indexOf("&", searchIdentifierStartIndex);
+        searchQuery = YouTubeURL.slice(searchIdentifierStartIndex, videoIdentifierEndIndex);
+    } else {
+        searchQuery = YouTubeURL.slice(searchIdentifierStartIndex);
+    }
+
+    // By default, the searchQuery content is encoded using Google's URL encoding scheme (https://developers.google.com/custom-search/docs/xml_results_appendices?hl=en#url-escaping)
+    // Hence we need to manually decode URL
+    return decodeGoogleSearchQuery(searchQuery);
+}
+
+// This function will decode Google's search URL query to original search content (https://developers.google.com/custom-search/docs/xml_results_appendices?hl=en#url-escaping)
+// First, we need to replace all instances of "+" with " " (a whitespace), and then we can run decodeURIComponent()
+const decodeGoogleSearchQuery = (googleSearchURL) => {
+    return decodeURIComponent(googleSearchURL.replaceAll("+", " "));
 }
 
 // Documentation that describe properties passed in to this parameter
@@ -34,9 +55,11 @@ const pageVisitListener = function(details) {
     // Page ID is unique on different page visiting (including refresh)
     // Tab ID is a non-decreasing integer, this Tab ID stays same in the same tab. Open a new tab will increase Tab ID, closing a Tab will not result a decreasing Tab ID on new tab creation
     // Window ID is a non-decreasing integer, this Window ID stays same in the same window (with a group of tabs), closing a Window will not result a decreasing Window ID on new Window creation
-    console.log("Page ID: " + details.pageId + ", Tab ID: " + details.tabId + ", windowID: " + details.windowId);
-    // console.log("PageVisitListener " + details.url + " from " + details.referrer); // Works as intended
+    // console.log("Page ID: " + details.pageId + ", Tab ID: " + details.tabId + ", windowID: " + details.windowId);
+    console.log("PageVisitListener " + details.url + " from " + details.referrer); // Works as intended
     // console.log("The time is " + details.pageVisitStartTime); // Will return timestamp (in millisecond) relative to UNIX epoch // Works as intended
+
+    console.log("Standardized YouTube URL is: " + standardizeYouTubeURL(details.url).content + " and " + standardizeYouTubeURL(details.url).type);
 }
 
 export function initialize() {
